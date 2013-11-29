93c64f53dd11d8b812e7dadec6b12cc814a9f147
davidfq <davidfq@gmail.com>
2013-11-29 19:41:07 +0100

    concept test

--- demo.js
+++ demo.js
@@ -1,12 +1,15 @@
 $(function(){
   
   // test page loading effect
-  var $body = $('body');
-  $body.barrita({
-    hideOnDone: false
-  });
-  setTimeout(function(){
-    $body.data('barrita').done();
-  }, 1000);
-  
+  // var $body = $('body');
+  // $body.barrita({
+  //   hideOnDone: false
+  // });
+  // setTimeout(function(){
+  //   $body.data('barrita').done();
+  // }, 1500);
+  $('#w1').barrita({ speed: 700 })
+  $('#w2').barrita({ speed: 400 })
+  $('#w3').barrita({ speed: 600 })
+  $('#w4').barrita({ speed: 400 })
 });
