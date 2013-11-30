21a3dee26c7679f17ea5d6e8a122fbf74c2f9245
davidfq <davidfq@gmail.com>
2013-11-30 15:04:01 +0100

    removes multi wrapper test, adds page load effect, links to rawgithub assets

--- demo.js
+++ demo.js
@@ -1,15 +1,11 @@
 $(function(){
   
-  // test page loading effect
-  // var $body = $('body');
-  // $body.barrita({
-  //   hideOnDone: false
-  // });
-  // setTimeout(function(){
-  //   $body.data('barrita').done();
-  // }, 1500);
-  $('#w1').barrita({ speed: 700 })
-  $('#w2').barrita({ speed: 400 })
-  $('#w3').barrita({ speed: 600 })
-  $('#w4').barrita({ speed: 400 })
+  // page load effect alla nprogress
+  var $body = $('body'),
+    barritaBody = $body.barrita().data('barrita');
+
+  setTimeout(function(){
+    barritaBody.done();
+  }, 1500);
+
 });
