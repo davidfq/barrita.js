692564cb2939887a429bb17423821866020d89f2
davidfq <davidfq@gmail.com>
2013-12-08 18:36:29 +0100

    first version

--- demo.js
+++ demo.js
@@ -1,11 +1,48 @@
 $(function(){
   
   // page load effect alla nprogress
-  var $body = $('body'),
-    barritaBody = $body.barrita().data('barrita');
+  // var $body = $('body'),
+  //   barritaBody = $body.barrita().data('barrita');
+  // 
+  // setTimeout(function(){
+  //   barritaBody.done();
+  // }, 1500);
 
-  setTimeout(function(){
-    barritaBody.done();
-  }, 1500);
+  var $startStop = $('#demo__display_start-stop'),
+    $randomInc = $('#demo__display_inc'),
+    $setVals = $('#demo__display_set-values'),
+    $settings = $('#demo__display_settings');
 
+  $startStop.barrita({
+    autoStart : false,
+    hideOnDone : false
+  });
+  
+  $randomInc.barrita({
+    autoStart : false,
+    hideOnDone : false
+  });
+  
+ $setVals.barrita({ autoStart : false });
+  
+  $settings.barrita({ 
+    speed : 2500,
+    trickleSpeed : 500,
+    hideOnDone : true,
+    autoStart : true
+  });
+  
+  $('button').on('click', function(e){
+    e.preventDefault();
+    var $el = $(this),
+      action = $el.data('action'),
+      $demo = $el.parents('.demo').find('.demo__display');
+    
+    $demo.data('barrita')[action]($el.data('arg'));
+  });
+  
+  $('.demo__code-trigger').on('click', function(e){
+    e.preventDefault();
+    $(this.hash).toggleClass('is-hidden');
+  });
 });
